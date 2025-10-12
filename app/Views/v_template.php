119d58e04d1ab8b9c905b0fb06d0f0a1499a9d53
askdhan <aska.dhanial@gmail.com>
2025-10-12 10:14:13 +0700

    Update v_template.php

--- app/Views/v_template.php
+++ app/Views/v_template.php
@@ -13,9 +13,9 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome Icons -->
-    <link rel="stylesheet" href="<?= base_url(relativePath: 'public/AdminLTE-3.2.0') ?>/plugins/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="<?= base_url('AdminLTE-3.2.0') ?>/plugins/fontawesome-free/css/all.min.css">
     <!-- Theme style -->
-    <link rel="stylesheet" href="<?= base_url(relativePath: 'public/AdminLTE-3.2.0') ?>/dist/css/adminlte.min.css">
+    <link rel="stylesheet" href="<?= base_url('AdminLTE-3.2.0') ?>/dist/css/adminlte.min.css">
 </head>
 
 <body class="hold-transition sidebar-mini">
@@ -360,11 +360,11 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- REQUIRED SCRIPTS -->
 
     <!-- jQuery -->
-    <script src="<?= base_url(relativePath: 'public/AdminLTE-3.2.0') ?>/plugins/jquery/jquery.min.js"></script>
+    <script src="<?= base_url('AdminLTE-3.2.0') ?>/plugins/jquery/jquery.min.js"></script>
     <!-- Bootstrap 4 -->
-    <script src="<?= base_url(relativePath: 'public/AdminLTE-3.2.0') ?>/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="<?= base_url('AdminLTE-3.2.0') ?>/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
     <!-- AdminLTE App -->
-    <script src="<?= base_url(relativePath: 'public/AdminLTE-3.2.0') ?>/dist/js/adminlte.min.js"></script>
+    <script src="<?= base_url('AdminLTE-3.2.0') ?>/dist/js/adminlte.min.js"></script>
 </body>
 
 </html>
