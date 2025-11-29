7ad289b06acb5ef0f21987b4ec065ccdc513f5ac
askdhan <aska.dhanial@gmail.com>
2025-11-29 07:34:14 +0700

    satuan crud

--- app/Controllers/Satuan.php
+++ app/Controllers/Satuan.php
@@ -2,8 +2,17 @@
 
 namespace App\Controllers;
 
+use App\Controllers\BaseController;
+use App\Models\ModelSatuan;
+
 class Satuan extends BaseController
 {
+    protected $ModelSatuan;
+
+    public function __construct()
+    {
+        $this->ModelSatuan = new ModelSatuan();
+    }
     public function index()
     {
         $data = [
@@ -12,6 +21,7 @@ class Satuan extends BaseController
             'menu' => 'data',
             'submenu' => 'satuan',
             'page' => 'v_satuan',
+            'satuan' => $this->ModelSatuan->AllData(),
         ];
         return view('v_template', $data);
     }
--- app/Views/v_satuan.php
+++ app/Views/v_satuan.php
@@ -1,19 +1,19 @@
- <div class="col-md-12">
-            <div class="card card-primary">
-              <div class="card-header">
-                <h3 class="card-title"><?= $subjudul ?></h3>
+<div class="col-md-12">
+  <div class="card card-primary">
+    <div class="card-header">
+      <h3 class="card-title"><?= $subjudul ?></h3>
 
-                <div class="card-tools">
-                  <button type="button" class="btn btn-tool"><i class="fas fa-plus"></i> Add Data
-                  </button>
-                </div>
-                <!-- /.card-tools -->
-              </div>
-              <!-- /.card-header -->
-              <div class="card-body">
-                The body of the card
-              </div>
-              <!-- /.card-body -->
-            </div>
-            <!-- /.card -->
-          </div>
+      <div class="card-tools">
+        <button type="button" class="btn btn-tool"><i class="fas fa-plus"></i> Add Data
+        </button>
+      </div>
+      <!-- /.card-tools -->
+    </div>
+    <!-- /.card-header -->
+    <div class="card-body">
+      The body of the card
+    </div>
+    <!-- /.card-body -->
+  </div>
+  <!-- /.card -->
+</div>
