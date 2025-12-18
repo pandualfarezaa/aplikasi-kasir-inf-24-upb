1ca27980e4586fc7b322f0a7061b197549cf56c2
askdhan <aska.dhanial@gmail.com>
2025-12-18 20:55:50 +0700

    CRUD Satuan

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname'     => 'localhost',
         'username'     => 'root',
         'password'     => '',
-        'database'     => 'db-kasir-ci4',
+        'database'     => 'db_kasir',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
--- app/Views/v_satuan.php
+++ app/Views/v_satuan.php
@@ -11,17 +11,27 @@
     </div>
     <!-- /.card-header -->
     <div class="card-body">
-      <table class="table table-responsive table-bordered">
-        <tr>
-          <th>No</th>
-          <th>Satuan</th>
-          <th>Aksi</th>
-        </tr>
-        <?php foreach ($satuan as $key => $value) { ?>
-          <tr>
-            
+      <table class="table table-bordered">
+        <thead>
+          <tr class="text-center">
+            <th width="50px">No</th>
+            <th>Satuan</th>
+            <th width="100px">Aksi</th>
           </tr>
-        <?php } ?>
+        </thead>
+        <tbody>
+          <?php $no = 1;
+          foreach ($satuan as $key => $value) { ?>
+            <tr>
+              <td><?= $no++ ?></td>
+              <td><?= $value['nama_satuan'] ?></td>
+              <td>
+                <button class="btn btn-warning btn-sm btn-flat "><i class="fas fa-pencil-alt"></i></button>
+                <button class="btn btn-danger btn-sm btn-flat"><i class="fas fa-trash"></i></button>
+              </td>
+            </tr>
+          <?php } ?>
+        </tbody>
       </table>
     </div>
     <!-- /.card-body -->
