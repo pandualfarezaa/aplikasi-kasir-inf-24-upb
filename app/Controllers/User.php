71bba1d7a78bbfe95bae0ae9006a5d860d923586
askdhan <aska.dhanial@gmail.com>
2025-12-24 15:50:41 +0700

    CRUD User

--- app/Controllers/User.php
+++ app/Controllers/User.php
@@ -2,8 +2,17 @@
 
 namespace App\Controllers;
 
+use App\Controllers\BaseController;
+use App\Models\ModelUser;
+
 class User extends BaseController
 {
+    protected $ModelUser;
+
+    public function __construct()
+    {
+        $this->ModelUser = new ModelUser();
+    }
     public function index()
     {
         $data = [
@@ -12,6 +21,7 @@ class User extends BaseController
             'menu' => 'data',
             'submenu' => 'user',
             'page' => 'v_user',
+            'user' => $this->ModelUser->AllData(),
         ];
         return view('v_template', $data);
     }
--- app/Views/v_user.php
+++ app/Views/v_user.php
@@ -1,11 +1,60 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User</title>
-</head>
-<body>
-    <h1>Halaman User</h1>
-</body>
-</html>
+<div class="col-md-12">
+    <div class="card card-primary">
+        <div class="card-header">
+            <h3 class="card-title"><?= $subjudul ?></h3>
+
+            <div class="card-tools">
+                <button type="button" class="btn btn-tool" data-toggle="modal" data-target="#add-data"><i
+                        class="fas fa-plus"></i> Add Data
+                </button>
+            </div>
+            <!-- /.card-tools -->
+        </div>
+        <!-- /.card-header -->
+        <div class="card-body">
+            <?php
+            if (session()->getFlashdata('pesan')) {
+                echo '<div class="alert alert-success alert-dismissible">
+                  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                  <h5><i class="icon fas fa-check"></i>';
+                echo session()->getFlashdata('pesan');
+                echo '</h5></div>';
+            }
+            ?>
+            <table class="table table-bordered">
+                <thead>
+                    <tr class="text-center">
+                        <th width="50px">No</th>
+                        <th>Nama User</th>
+                        <th>Email</th>
+                        <th>Password</th>
+                        <th>Level</th>
+                        <th width="100px">Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php $no = 1;
+                    foreach ($user as $key => $value) { ?>
+                        <tr>
+                            <td><?= $no++ ?></td>
+                            <td><?= $value['nama_user'] ?></td>
+                            <td><?= $value['email'] ?></td>
+                            <td><?= $value['password'] ?></td>
+                            <td><?= $value['level'] ?></td>
+                            <td>
+                                <button class="btn btn-warning btn-sm btn-flat" data-toggle="modal"
+                                    data-target="#edit-data<?= $value['id_user'] ?>"><i
+                                        class="fas fa-pencil-alt"></i></button>
+                                <button class="btn btn-danger btn-sm btn-flat" data-toggle="modal"
+                                    data-target="#delete-data<?= $value['id_user'] ?>"><i
+                                        class="fas fa-trash"></i></button>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
+        <!-- /.card-body -->
+    </div>
+    <!-- /.card -->
+</div>
