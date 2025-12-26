d382e6a18e82bdb49d04cb79721e0a2abd43da88
lanjar86 <lanjaradi09@gmail.com>
2025-12-26 19:44:24 +0700

    Perbarui konfigurasi database

--- app/Views/v_admin.php
+++ app/Views/v_admin.php
@@ -57,3 +57,90 @@
         <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
     </div>
 </div>
+
+
+   <div class="col-md-4">
+            <!-- Info Boxes Style 2 -->
+            <div class="info-box mb-3 bg-maroon">
+              <span class="info-box-icon"><i class="fa-solid fa-dollar-sign"></i></span>
+
+              <div class="info-box-content">
+                <span class="info-box-text">Income</span>
+                <span class="info-box-number">Rp. 5,200</span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+</div>
+
+ <div class="col-md-4">
+            <!-- Info Boxes Style 2 -->
+            <div class="info-box mb-3 bg-indigo">
+              <span class="info-box-icon"><i class="fa-solid fa-dollar-sign"></i></span>
+
+              <div class="info-box-content">
+                <span class="info-box-text">Income Bulan Ini</span>
+                <span class="info-box-number">Rp. 5,200</span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+</div>
+
+ <div class="col-md-4">
+            <!-- Info Boxes Style 2 -->
+            <div class="info-box mb-3 bg-orange">
+              <span class="info-box-icon"><i class="fa-solid fa-dollar-sign"></i></span>
+
+              <div class="info-box-content">
+                <span class="info-box-text">Income Tahun Ini</span>
+                <span class="info-box-number">Rp. 5,200</span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+</div>
+
+
+<div class="col-md-12">
+    <canvas id="myChart" width="100%" height="35px"></canvas>
+</div>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+<script>
+  const ctx = document.getElementById('myChart');
+  const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+</script>
