000749d6aaadcf5043cdcb79cd5254b8a7ff177e
askdhan <aska.dhanial@gmail.com>
2025-12-28 14:47:02 +0700

    menampilkan produk

--- app/Controllers/Produk.php
+++ app/Controllers/Produk.php
@@ -2,8 +2,17 @@
 
 namespace App\Controllers;
 
+use App\Controllers\BaseController;
+use App\Models\ModelProduk;
+
 class Produk extends BaseController
 {
+    protected $ModelProduk;
+
+    public function __construct()
+    {
+        $this->ModelProduk = new ModelProduk();
+    }
     public function index()
     {
         $data = [
@@ -12,7 +21,37 @@ class Produk extends BaseController
             'menu' => 'data',
             'submenu' => 'produk',
             'page' => 'v_produk',
+            'produk' => $this->ModelProduk->AllData(),
         ];
-        return view('v_template', $data); 
+        return view('v_template', $data);
+    }
+
+    public function InsertData()
+    {
+        $data = ['nama_satuan' => $this->request->getPost('nama_satuan')];
+        $this->ModelSatuan->InsertData($data);
+        session()->setFlashdata('pesan', 'Data Berhasil Ditambahkan!!');
+        return redirect()->to(base_url('public/satuan'));
+    }
+
+    public function UpdateData($id_satuan)
+    {
+        $data = [
+            'id_satuan' => $id_satuan,
+            'nama_satuan' => $this->request->getPost('nama_satuan')
+        ];
+        $this->ModelSatuan->UpdateData($data);
+        session()->setFlashdata('pesan', 'Data Berhasil Diedit!!');
+        return redirect()->to(base_url('public/satuan'));
+    }
+
+    public function DeleteData($id_satuan)
+    {
+        $data = [
+            'id_satuan' => $id_satuan
+        ];
+        $this->ModelSatuan->DeleteData($data);
+        session()->setFlashdata('pesan', 'Data Berhasil Dihapus!!');
+        return redirect()->to(base_url('public/satuan'));
     }
 }
--- app/Views/v_produk.php
+++ app/Views/v_produk.php
@@ -1,11 +1,66 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Produk</title>
-</head>
-<body>
-    <h1>Halaman Produk</h1>
-</body>
-</html>
+<div class="col-md-12">
+    <div class="card card-primary">
+        <div class="card-header">
+            <h3 class="card-title"><?= $subjudul ?></h3>
+
+            <div class="card-tools">
+                <button type="button" class="btn btn-tool" data-toggle="modal" data-target="#add-data"><i
+                        class="fas fa-plus"></i> Add Data
+                </button>
+            </div>
+            <!-- /.card-tools -->
+        </div>
+        <!-- /.card-header -->
+        <div class="card-body">
+            <?php
+            if (session()->getFlashdata('pesan')) {
+                echo '<div class="alert alert-success alert-dismissible">
+                  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                  <h5><i class="icon fas fa-check"></i>';
+                echo session()->getFlashdata('pesan');
+                echo '</h5></div>';
+            }
+            ?>
+            <table class="table table-bordered">
+                <thead>
+                    <tr class="text-center">
+                        <th width="50px">No</th>
+                        <th>Kode/Barcode</th>
+                        <th>Nama Produk</th>
+                        <th>Kategori</th>
+                        <th>Satuan</th>
+                        <th>Harga Beli</th>
+                        <th>Harga Jual</th>
+                        <th>Stok</th>
+                        <th width="100px">Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php $no = 1;
+                    foreach ($produk as $key => $value) { ?>
+                        <tr>
+                            <td><?= $no++ ?></td>
+                            <td  class="text-center"><?= $value['kode_produk'] ?></td>
+                            <td><?= $value['nama_produk'] ?></td>
+                            <td class="text-center"><?= $value['nama_kategori'] ?></td>
+                            <td class="text-center"><?= $value['nama_satuan'] ?></td>
+                            <td class="text-right">Rp. <?= number_format($value['harga_beli'], 0) ?></td>
+                            <td class="text-right">Rp. <?= number_format($value['harga_jual'], 0) ?></td>
+                            <td class="text-center"><?= $value['stok'] ?></td>
+                            <td class="text-center">
+                                <button class="btn btn-warning btn-sm btn-flat" data-toggle="modal"
+                                    data-target="#edit-data<?= $value['id_produk'] ?>"><i
+                                        class="fas fa-pencil-alt"></i></button>
+                                <button class="btn btn-danger btn-sm btn-flat" data-toggle="modal"
+                                    data-target="#delete-data<?= $value['id_produk'] ?>"><i
+                                        class="fas fa-trash"></i></button>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
+        <!-- /.card-body -->
+    </div>
+    <!-- /.card -->
+</div>
