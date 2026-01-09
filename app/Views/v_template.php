178d253eee3895d1d485bc769fa0e5548c53fbeb
askdhan <aska.dhanial@gmail.com>
2026-01-09 09:29:24 +0700

    edit profil kasir

--- app/Views/v_template.php
+++ app/Views/v_template.php
@@ -112,10 +112,10 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <!-- Main Sidebar Container -->
         <aside class="main-sidebar sidebar-dark-warning elevation-4">
             <!-- Brand Logo -->
-            <a href="index3.html" class="brand-link">
-                <img src="<?= base_url('public/AdminLTE/dist/img/AdminLTELogo.png') ?>" alt="AdminLTE Logo"
+            <a href="<?= base_url('public/AdminLTE/dist/img/jmk48.jpg') ?>" class="brand-link">
+                <img src="<?= base_url('public/AdminLTE/dist/img/jmk48.jpg') ?>" alt="AdminLTE Logo"
                     class="brand-image img-circle elevation-3" style="opacity: .8">
-                <span class="brand-text font-weight-light">Kasir</span>
+                <span class="brand-text font-weight-light">Kasir JMK48</span>
             </a>
 
             <!-- Sidebar -->
