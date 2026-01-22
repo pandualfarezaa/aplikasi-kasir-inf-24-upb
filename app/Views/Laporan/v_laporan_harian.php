103f425c5b11443bef1a662279201fa7d1f025b7
pandualfarezaa <pandualfareza7@gmail.com>
2026-01-22 22:24:42 +0700

    Update v_laporan_harian.php

--- app/Views/Laporan/v_laporan_harian.php
+++ app/Views/Laporan/v_laporan_harian.php
@@ -1,36 +1,39 @@
-<div class="col-md-12">
-    <div class="card card-info">
-        <div class="card-header">
-            <h3 class="card-title">Laporan Harian</h3>
-        </div>
-        <div class="card-body">
-            <div class="row">
-                <div class="col-sm-5">
-                    <div class="form-group row">
-                        <label class="col-sm-2.5 col-form-label">Tanggal :</label>
-                        <div class="col-sm-10 input-group">
-                            <input type="date" name="tgl" id="tgl" class="form-control" placeholder="Tanggal">
-                                <div class="input-group-append">
-                                    <button onclick="ViewTabelLaporan()" class="btn btn-primary btn-flat" type="button">
-                                        <i class="fas fa-file-alt"></i> view laporan
-                                    </button>
-                                    <button class="btn btn-danger btn-flat" type="button">
-                                        <i class="fas fa-print"></i> print
-                                    </button>
-                                </div>
+<div class="card card-info">
+    <div class="card-header">
+        <h3 class="card-title">Laporan Harian</h3>
+    </div>
+
+    <div class="card-body">
+        <div class="row">
+            <div class="col-sm-5">
+                <div class="form-group row">
+                    <label class="col-sm-3 col-form-label">Tanggal</label>
+                    <div class="col-sm-9 input-group">
+                        <input type="date" id="tgl" class="form-control">
+
+                        <div class="input-group-append">
+                            <button type="button"
+                                    class="btn btn-primary"
+                                    onclick="ViewTabelLaporan()">
+                                <i class="fas fa-file-alt"></i> View
+                            </button>
+
+                            <button type="button"
+                                    class="btn btn-danger"
+                                    onclick="PrintLaporan()">
+                                <i class="fas fa-print"></i> Print
+                            </button>
                         </div>
                     </div>
-                </div>   
+                </div>
             </div>
         </div>
-        <div class="col-sm-12">
-            <hr>
-            <div class="tabel">
 
-            </div>
-        </div>
+        <hr>
+        <div class="tabel"></div>
     </div>
-</div>    
+</div>
+
 
 
 <script>
